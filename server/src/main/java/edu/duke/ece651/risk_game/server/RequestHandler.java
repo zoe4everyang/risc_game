20313c749d9aeeb491cfd9bab5d26154c5eb460d
zy162 <zy162@duke.edu>
2023-04-03 14:04:49 +0800

    add requestHandler

---
+++ server/src/main/java/edu/duke/ece651/risk_game/server/RequestHandler.java
@@ -0,0 +1,12 @@
+package edu.duke.ece651.risk_game.server;
+
+import java.io.*;
+
+public class RequestHandler {
+    Controller controller;
+
+    public RequestHandler(){
+
+    }
+
+}
--- server/src/main/java/edu/duke/ece651/risk_game/server/ServerApplication.java
+++ server/src/main/java/edu/duke/ece651/risk_game/server/ServerApplication.java
@@ -1,4 +1,6 @@
 package edu.duke.ece651.risk_game.server;
+import java.io.*;
+import java.net.*;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServerApplication {
 
+
     public static void main(String[] args) {
+
         SpringApplication.run(ServerApplication.class, args);
     }
 

3177c9a58b871c3bbb5686981f37be1bfdcb3a97
zgw2000 <zengguowang2000@gmail.com>
2023-04-03 23:09:48 +0800

    Add I/O

---
+++ client/src/main/java/edu/duke/ece651/risk_game/client/InputController.java
@@ -0,0 +1,28 @@
+package edu.duke.ece651.risk_game.client;
+
+import java.io.InputStream;
+
+public class InputController {
+    private RISCClient httpClient;
+    private InputStream input;
+    private ClientChecker checker;
+    private Viewer riskViewer;
+    private Message inputMessage;
+
+    public void startGame() {
+
+    }
+
+    private void initPhase() {
+
+    }
+
+    private void placementPhase() {
+
+    }
+
+    private void gamePhase() {
+
+    }
+
+}

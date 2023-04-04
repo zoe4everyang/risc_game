02262b1cc27de3ff2a92c53926dd3a7d057d159b
LeeTimeTrue1999 <129301730+LeeTimeTrue1999@users.noreply.github.com>
2023-04-04 22:46:04 +0800

    add test cases

--- server/src/main/java/edu/duke/ece651/risk_game/server/Checker.java
+++ server/src/main/java/edu/duke/ece651/risk_game/server/Checker.java
@@ -3,15 +3,15 @@ package edu.duke.ece651.risk_game.server;
 public class Checker {
     public Boolean checkMove(int playerId, int from, int to, int num, WorldMap map) {
         if (map.getTerritories().get(from).getOwner() != playerId || map.getTerritories().get(to).getOwner() != playerId) {
-            throw new IllegalArgumentException("you are moving from/to a territory not belong to you!");
+            //throw new IllegalArgumentException("you are moving from/to a territory not belong to you!");
             return false;
         }
         if (!map.isConnected(from, to, playerId)) {
-            throw new IllegalArgumentException("you can not moving to a unconnect territory!");
+            //throw new IllegalArgumentException("you can not moving to a unconnect territory!");
             return false;
         }
         if (num > map.getTerritories().get(from).getUnits() - 1) {
-            throw new IllegalArgumentException("you do not have enough units to make the movement!");
+            //throw new IllegalArgumentException("you do not have enough units to make the movement!");
             return false;
         }
         return true;   
@@ -19,7 +19,7 @@ public class Checker {
 
     public Boolean checkAttackTarget(int playerId, int from , int to, int num, WorldMap map) {
         if (map.getTerritories().get(from).getOwner() != playerId || map.getTerritories().get(to).getOwner() == playerId) {
-            throw new IllegalArgumentException("you can not attack yourself!!");
+            //throw new IllegalArgumentException("you can not attack yourself!!");
             return false;
         }
         return true;
@@ -30,11 +30,11 @@ public class Checker {
             return false;
         }
         if (!map.isNeighbour(from, to)) {
-            throw new IllegalArgumentException("you can only attack the connected territory!");
+            //throw new IllegalArgumentException("you can only attack the connected territory!");
             return false;
         }
         if (num > map.getTerritories().get(from).getUnits() - 1) {
-            throw new IllegalArgumentException("you do not have enough units to attack!");
+            //throw new IllegalArgumentException("you do not have enough units to attack!");
             return false;
         }
         return true;
@@ -42,8 +42,8 @@ public class Checker {
 
     // TODO: check the new check method
     public Boolean checkUsable(int playerId, int from, int to, int num, WorldMap map) {
-       if(map.checkOnMap(to) == false && checkAttack(num) == false) {
-            throw new IllegalArgumentException("you are trying to move from/to a territory which does not exist!");
+       if(map.checkOnMap(to) == false || map.checkOnMap(from) == false) {
+            //throw new IllegalArgumentException("you are trying to move from/to a territory which does not exist!");
             return false;
        }
     }

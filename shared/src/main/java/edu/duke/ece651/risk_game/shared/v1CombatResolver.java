3d53c61d9444c75d96085b96451ad2d64d9cab94
fqzz2000 <fqzz2000@outlook.com>
2023-04-05 13:24:42 +0800

    copy files to shared directory

---
+++ shared/src/main/java/edu/duke/ece651/risk_game/shared/v1CombatResolver.java
@@ -0,0 +1,40 @@
+package edu.duke.ece651.risk_game.server;
+
+import java.util.Random;
+
+public class v1CombatResolver implements CombatResolver {
+    
+    private int seed;
+
+    private Random generator;
+
+    v1CombatResolver() {
+        seed = (int) Math.random() * 5000;
+        generator = new Random(seed);
+    }
+
+    public int resolveCombat(int attackNum, int defendNum) {
+        // return the number of units that survive, positive indicate attacker wins, negative indicate defender wins
+        // the absolute value of return value indicate the number of units that survive
+        while (attackNum > 0 && defendNum > 0) {
+            // roll two 20-sided dice for attacker and defender
+            // the one with lower number loses one unit
+            int attackRoll = (int) (generator.nextDouble() * 20) + 1;
+            int defendRoll = (int) (generator.nextDouble() * 20) + 1;
+            if (attackRoll > defendRoll) {
+                defendNum--;
+            } else {
+                attackNum--;
+            }
+        }
+        return attackNum - defendNum;
+    }
+
+    public void setSeed(int seed) {
+        // set the seed for random number generator
+        this.seed = seed;
+        // set seed for Math.random
+        generator = new Random(seed);
+    }
+    
+}
---
+++ shared/src/main/java/edu/duke/ece651/risk_game/shared/v1Territory.java
@@ -0,0 +1,71 @@
+package edu.duke.ece651.risk_game.server;
+import java.util.List;
+
+public class v1Territory implements Territory{
+    private int id;
+    private String name;
+    private int owner;
+    private int units;
+    private List<Integer> distances;
+    private CombatResolver combatResolver;
+
+    v1Territory(int id, String name, int owner, List<Integer> distances, CombatResolver combatResolver) {
+        // constructor
+        this.id = id;
+        this.name = name;
+        this.owner = owner;
+        this.units = 0;
+        this.distances = distances;
+        this.combatResolver = combatResolver;
+    }
+
+
+    public void addUnit(int unit) {
+        // add unit to territory
+        this.units += unit;
+    }
+
+    public void removeUnit(int unit) {
+        // remove unit from territory
+        this.units -= unit;
+    }
+    
+    public void defence(int attacker, int unit) {
+        // defend against attacker
+        int result = combatResolver.resolveCombat(unit, this.units);
+        if (result > 0) {
+            // attacker wins
+            this.owner = attacker;
+            this.units = result;
+        } else if (result < 0) {
+            // defender wins
+            this.units = -result;
+        }
+    }
+    
+    public List<Integer> getDistances() {
+        // return list of distances to other territories
+        return distances;
+    }
+
+    public int getID() {
+        // return territory ID
+        return id;
+    }
+
+    public String getName() {
+        // return territory name
+        return name;
+    }
+
+    public int getOwner() {
+        // return territory owner
+        return owner;
+    }
+
+    public int getUnits() {
+        // return number of units in territory
+        return units;
+    }
+
+}

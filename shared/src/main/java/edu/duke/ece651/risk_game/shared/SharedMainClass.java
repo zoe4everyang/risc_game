b0b618645ff2b5eae250a3b13d53a98590cc3324
Tenki99 <tenki.qiang@gmail.com>
2023-04-06 17:57:17 +0800

    build repair

---
+++ shared/src/main/java/edu/duke/ece651/risk_game/shared/SharedMainClass.java
@@ -0,0 +1,11 @@
+package edu.duke.ece651.risk_game.shared;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedMainClass {
+    public static void main(String[] args) {
+        SpringApplication.run(SharedMainClass.class, args);
+    }
+}

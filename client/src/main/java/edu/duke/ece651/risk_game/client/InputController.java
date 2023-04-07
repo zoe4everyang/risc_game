4f7b7d9fe4319181fd0b57dc2caa0811ea4b7431
zgw2000 <zengguowang2000@gmail.com>
2023-04-07 19:05:48 +0800

    modify InputController

--- client/src/main/java/edu/duke/ece651/risk_game/client/InputController.java
+++ client/src/main/java/edu/duke/ece651/risk_game/client/InputController.java
@@ -1,7 +1,9 @@
 package edu.duke.ece651.risk_game.client;
 import edu.duke.ece651.risk_game.shared.Message;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 public class InputController {
     private RISCClient httpClient;
@@ -10,20 +12,68 @@ public class InputController {
     private Viewer riskViewer;
     private Message inputMessage;
 
-    public void startGame() {
+    public InputController(InputStream input, ClientChecker checker, Viewer riskViewer) {
+        this.httpClient = new RISCClient();
+        this.input = input;
+        this.checker = checker;
+        this.riskViewer = riskViewer;
+    }
 
+    public void startGame() {
+        try {
+            InitResponse initResponse = httpClient.sendStart();
+            riskViewer.update(initResponse.getTerritories());
+        } catch (IOException e) {
+            System.out.println("Error while sending start request: " + e.getMessage());
+        }
     }
 
     private void initPhase() {
+        List<Integer> unitPlacement = checker.getUnitPlacement(input);
+        PlacementRequest placementRequest = new PlacementRequest(checker.getPlayerId(), unitPlacement);
 
+        try {
+            Response response = httpClient.sendPlacement(placementRequest);
+            riskViewer.update(response.getTerritories());
+        } catch (IOException e) {
+            System.out.println("Error while sending placement request: " + e.getMessage());
+        }
     }
 
     private void placementPhase() {
+        List<Integer> unitPlacement = checker.getUnitPlacement(input);
+        PlacementRequest placementRequest = new PlacementRequest(checker.getPlayerId(), unitPlacement);
 
+        try {
+            Response response = httpClient.sendPlacement(placementRequest);
+            riskViewer.update(response.getTerritories());
+        } catch (IOException e) {
+            System.out.println("Error while sending placement request: " + e.getMessage());
+        }
     }
 
     private void gamePhase() {
+        while (true) {
+            ActionRequest actionRequest = checker.getActionRequest(input);
 
+            try {
+                Response response = httpClient.sendAction(actionRequest);
+                riskViewer.update(response.getTerritories());
+
+                if (response.isEnd()) {
+                    System.out.println("Game Over");
+                    break;
+                }
+            } catch (IOException e) {
+                System.out.println("Error while sending action request: " + e.getMessage());
+            }
+        }
     }
 
+    public void run() {
+        startGame();
+        initPhase();
+        placementPhase();
+        gamePhase();
+    }
 }

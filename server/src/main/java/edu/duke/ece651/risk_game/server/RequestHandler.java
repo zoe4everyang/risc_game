fd778fd35d78e69692ae0609a39fccbe121e43db
zy162 <zy162@duke.edu>
2023-04-08 13:30:45 +0800

    refactor handler & add initializing of unitAvailable

--- server/src/main/java/edu/duke/ece651/risk_game/server/RequestHandler.java
+++ server/src/main/java/edu/duke/ece651/risk_game/server/RequestHandler.java
@@ -8,6 +8,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class RequestHandler {
     private Controller controller;
     private AtomicInteger count;
+    private AtomicInteger exit;
     private int playerNum;
     //private Queue<Message> msgList;
     private int registerID;
@@ -25,11 +26,14 @@ public class RequestHandler {
         this.playerNum = playerNum;
         controller = new Controller(playerNum);
         count = new AtomicInteger(0);
+        exit = new AtomicInteger(0);
         //:msgList = new LinkedList<>();
         registerID = -1;
+        attackPlayers = new ArrayList<>();
         attackFrom = new ArrayList<>();
         attackTo = new ArrayList<>();
         attackNum = new ArrayList<>();
+        movePlayers = new ArrayList<>();
         moveFrom = new ArrayList<>();
         moveTo = new ArrayList<>();
         moveNum = new ArrayList<>();
@@ -40,12 +44,14 @@ public class RequestHandler {
         synchronized (this) {
             count.incrementAndGet();
             playerID = registerPlayer();
-            if (count.get() < playerNum) {
+            exit.set(0);
+            while (count.get() < playerNum && exit.get() == 0) {
                 wait();
-                System.out.println(count.get());
-            } else {
-                notifyAll();
+            }
+            if(count.get() == playerNum){
+                exit.set(1);
                 count.set(0);
+                notifyAll();
             }
         }
         int unitAvailable = controller.getUnitAvailable();
@@ -64,11 +70,15 @@ public class RequestHandler {
         synchronized (this) {
             List<Integer> unitPlacement = msg.getPlacement();
             controller.initGame(unitPlacement);
+            if(count.get() == playerNum) {
+                count.set(0);
+            }
             count.incrementAndGet();
             if (count.get() < playerNum) {
-                wait();
+                while (count.get() < playerNum) {
+                    wait();
+                }
             } else {
-                count.set(0);
                 notifyAll();
             }
         }
@@ -79,34 +89,45 @@ public class RequestHandler {
 
     // move & attack
     public Message operationHandler(ActionRequest msg) throws InterruptedException{
-
-        Boolean isGameEnd;
+        //Boolean isGameEnd;
         int playerID = msg.getPlayerID();
         synchronized (this) {
+            if(count.get() == playerNum) {
+                count.set(0);
+            }
             count.incrementAndGet();
-            for(int i = 0; i < msg.getAttackFrom().size(); i++){
-                attackPlayers.add(playerID);
-                attackFrom.add(msg.getAttackFrom().get(i));
-                attackTo.add(msg.getAttackTo().get(i));
-                attackNum.add(msg.getAttackNums().get(i));
-            }
-            for(int i = 0; i < msg.getMoveFrom().size(); i++){
-                movePlayers.add(playerID);
-                moveFrom.add(msg.getMoveFrom().get(i));
-                moveTo.add(msg.getMoveTo().get(i));
-                moveNum.add(msg.getMoveNums().get(i));
-            }
+            attackPlayers.addAll(Collections.nCopies(playerID, msg.getAttackFrom().size()));
+            attackFrom.addAll(msg.getAttackFrom());
+            attackTo.addAll(msg.getAttackTo());
+            attackNum.addAll(msg.getAttackNums());
+
+            movePlayers.addAll(Collections.nCopies(playerID, msg.getMoveFrom().size()));
+            moveFrom.addAll(msg.getMoveFrom());
+            moveTo.addAll(msg.getMoveTo());
+            moveNum.addAll(msg.getMoveNums());
+            //            for(int i = 0; i < msg.getAttackFrom().size(); i++){
+//                attackPlayers.add(playerID);
+//                attackFrom.add(msg.getAttackFrom().get(i));
+//                attackTo.add(msg.getAttackTo().get(i));
+//                attackNum.add(msg.getAttackNums().get(i));
+//            }
+//            for(int i = 0; i < msg.getMoveFrom().size(); i++){
+//                movePlayers.add(playerID);
+//                moveFrom.add(msg.getMoveFrom().get(i));
+//                moveTo.add(msg.getMoveTo().get(i));
+//                moveNum.add(msg.getMoveNums().get(i));
+//            }
             if (count.get() < playerNum) {
-                wait();
+                while(count.get() < playerNum) {
+                    wait();
+                }
             } else {
                 controller.step(attackPlayers, attackFrom, attackTo, attackNum,
                         movePlayers, moveFrom, moveTo, moveNum);
-                count.set(0);
                 notifyAll();
             }
-
         }
-        List<Territory> territories = new ArrayList<>();
+        List<Territory> territories = controller.getTerritories();
         Boolean isPlayerLose = controller.checkLose(playerID);
         Message response = new Response(playerID, territories, isPlayerLose, controller.checkEnd());
         return response;

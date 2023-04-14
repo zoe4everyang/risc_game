3fef51674deb947c11bd7e72f6bfb1c706039b36
fqzz2000 <fqzz2000@outlook.com>
2023-04-14 23:53:49 +0800

    add troop class without test

--- shared/src/main/java/edu/duke/ece651/risk_game/shared/Troop.java
+++ shared/src/main/java/edu/duke/ece651/risk_game/shared/Troop.java
@@ -1,7 +1,18 @@
 package edu.duke.ece651.risk_game.shared;
 
+import java.util.List;
+
 public interface Troop {
     public int getOwner();
+
+    public void deleteUnit(int unitId);
+    public void addUnit(Unit unit);
+
+    public void addUnits(List<Unit> units);
+
+    public void setOwner(int owner);
     public boolean upgrade(int unitId);
+
+    public List<Unit> getUnits();
 
 }
--- shared/src/main/java/edu/duke/ece651/risk_game/shared/unitTroop.java
+++ shared/src/main/java/edu/duke/ece651/risk_game/shared/unitTroop.java
@@ -9,6 +9,35 @@ public class unitTroop implements Troop{
     public int getOwner() {
         return owner;
     }
+    @Override
+    public List<Unit> getUnits() {
+        return units;
+    }
+
+    @Override
+    public void deleteUnit(int unitId) {
+        for (int i = 0; i < units.size(); i++) {
+            if (units.get(i).getUnitId() == unitId) {
+                units.remove(i);
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void addUnit(Unit unit) {
+        units.add(unit);
+    }
+
+    @Override
+    public void addUnits(List<Unit> units) {
+        this.units.addAll(units);
+    }
+
+    @Override
+    public void setOwner(int owner) {
+        this.owner = owner;
+    }
 
     @Override
     public boolean upgrade(int unitId) {
